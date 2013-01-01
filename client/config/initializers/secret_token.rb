be9072b136d3b4ee94469efcdb4058eed577c118
Annabelle Richard <annabelle.e.richard@gmail.com>
2013-01-01 11:49:28 -0800

    Initial client commit.

---
+++ client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Client::Application.config.secret_token = 'ab3034a876f290d0ba4a3868cdb583ab29bca112e1c9259eb79c7da05aeebda316e032c08f97b5bdb91d244880621d25c83cb5d65655211ea3f0fc3300672016'
